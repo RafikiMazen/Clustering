d97aacd54be4652b24781863a6088d24af843344
RafikiMazen <47504028+RafikiMazen@users.noreply.github.com>
2020-05-22 00:09:11 +0200

    updated from K-means to Fuzzy c-means

--- clustering.py
+++ clustering.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from shapely.geometry import Point,Polygon
 from sklearn.cluster import KMeans
+from fcmeans import FCM
 
 
 
@@ -13,13 +14,14 @@ df = pd.read_csv('./pollution data/combined_dataset.csv', names = colnames)
 
 # plt.scatter(x=df['longitude'], y=df['latitude'])
 # plt.show()
-kmeans = KMeans(n_clusters=4)
-kmeans.fit(df[['longitude','latitude']])
-y_kmeans = kmeans.predict(df[['longitude','latitude']])
-plt.scatter(df['longitude'],y=df['latitude'], c=y_kmeans, s=50, cmap='viridis')
-df['cluster_label']=y_kmeans
+numOfClusters=4
+fcm = FCM(n_clusters=numOfClusters)
+fcm.fit(map[['longitude', 'latitude']])
+y_fcmeans = fcm.predict(df[['longitude','latitude']])
+plt.scatter(df['longitude'],y=df['latitude'], c=y_fcmeans, s=50, cmap='viridis')
+df['cluster_label']=y_fcmeans
 
-centers = kmeans.cluster_centers_
+centers = fcm.centers_
 plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5)
 plt.show()
 # df.to_csv ('clustered_data.csv', index=None, header = True)
